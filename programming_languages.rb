78f17403edd73654f4578dc19055ef13a079ccbd
Jeffrey Crockett <jeffreyneilcrockett@gmail.com>
2018-07-02 21:18:41 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -44,10 +44,14 @@ def reformat_languages(languages)
   languages.each do |style, language_hash|
     new_hash.each do |language_name, language_stats|
       if language_hash.keys.include?(language_name)
-      
         language_stats[:style] << style
       end
-      language_stats[:type] = language_hash[:type]
+    end
+  end
+  
+  languages.each do |style, language_hash|
+    language_hash.each do |language_name, style_hash|
+      new_hash[:type] = style_hash[:type]
     end
   end
   binding.pry
