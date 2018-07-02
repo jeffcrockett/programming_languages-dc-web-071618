d3d31de64197b4acc32298d989e4b4b010ddacba
Jeffrey Crockett <jeffreyneilcrockett@gmail.com>
2018-07-02 20:48:54 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -33,11 +33,11 @@ languages = {
 }
 
 def reformat_languages(languages)
-  binding.pry
   new_hash = {}
   languages.each do |style, lang|
     lang.each do |name, info|
       new_hash[:language_name] = {}
     end
   end
+    binding.pry
 end
